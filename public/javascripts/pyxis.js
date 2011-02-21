43a6383f3f8494beeb37c754c74d6c6406bc6e0d
Matthias Bauer <moeffju@gmail.com>
2011-02-21 16:20:03 +0100

    Fix console.log errors, duh

--- public/javascripts/pyxis.js
+++ public/javascripts/pyxis.js
@@ -1,3 +1,5 @@
+if(typeof(console) === 'undefined'){var console = {};console.log = console.error = console.info = console.debug = console.warn = console.trace = console.dir = console.dirxml = console.group = console.groupEnd = console.time = console.timeEnd = console.assert = console.profile = function() {};}
+
 Pyxis = {
   contentMode: 'link',
   
