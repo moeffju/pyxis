8a898aee2b414c3d56f13b32153ffe20110cb750
Matthias Bauer <moeffju@gmail.com>
2011-02-21 16:20:20 +0100

    Fix console.log errors and typo

--- javascripts/pyxis.js
+++ javascripts/pyxis.js
@@ -1,6 +1,6 @@
 if(typeof(console) === 'undefined'){var console = {};console.log = console.error = console.info = console.debug = console.warn = console.trace = console.dir = console.dirxml = console.group = console.groupEnd = console.time = console.timeEnd = console.assert = console.profile = function() {};}
 
-logPyxis = {
+Pyxis = {
   contentMode: 'link',
   
   setContentMode: function(m) {
